a28aba29b56babc75aaaf72d1a9f9db45822cbd7
danimothman <hwankook.kim9@gmail.com>
2020-07-07 15:51:14 +0900

    test1

---
+++ routes/Router.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+var path  = require('path')
+var fs = require('fs')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+require('dotenv').config()
+var apiRouter  = require('./routes/Router')
+// view template  setting
+app.set('views', path.resolve(__dirname+'/views'))
+app.set('view engine' , 'ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+//cors 설정
+app.use(cors())
+//routing file 등록
+app.use('/', apiRouter)
+var port = process.env.PORT || 3000
+app.listen(port , ()=>{
+    console.log(`Server is Starting at http://localhost:${port}`)
+})
